ecfbca164c01fccdf49aba2ab4d904996ba95f93
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-04 22:21:42 +0800

    added farm foreign key to container table

--- src/main/resources/db/migration/V19__create_container_table.sql
+++ src/main/resources/db/migration/V19__create_container_table.sql
@@ -2,8 +2,10 @@ CREATE TABLE container(
     id INT NOT NULL AUTO_INCREMENT,
     arduino_board_id INT,
     plant_id INT,
+    farm_id INT,
     name VARCHAR(20) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (arduino_board_id) REFERENCES arduino_board(id),
-    FOREIGN KEY (plant_id) REFERENCES plant(id)
+    FOREIGN KEY (plant_id) REFERENCES plant(id),
+    FOREIGN KEY (farm_id) REFERENCES farm(id)
 );
