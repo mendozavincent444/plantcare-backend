e97ef3ccb868fd6e469f52dd71ade07ac97825aa
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-05 16:11:20 +0800

    added foreign key to farm table creation script

--- src/main/resources/db/migration/V12__create_farm_table.sql
+++ src/main/resources/db/migration/V12__create_farm_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE farm(
     id INT NOT NULL AUTO_INCREMENT,
-    room_temp_and_humidity_sensor_id INT NULL,
+    room_temp_and_humidity_sensor_id INT,
     location VARCHAR(255) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (room_temp_and_humidity_sensor_id) REFERENCES sensor(id)
 );
