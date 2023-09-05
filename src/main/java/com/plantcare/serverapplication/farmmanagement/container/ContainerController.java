3d30256699a491d4f827b2e5fb87bec629cd32d8
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-05 15:43:58 +0800

    changed columns to accept more values DECIMAL(5,2) and NULL

--- src/main/java/com/plantcare/serverapplication/farmmanagement/container/ContainerController.java
+++ src/main/java/com/plantcare/serverapplication/farmmanagement/container/ContainerController.java
@@ -1,7 +1,5 @@
 package com.plantcare.serverapplication.farmmanagement.container;
 
-import org.apache.coyote.Response;
-import org.hibernate.event.spi.PostDeleteEvent;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/resources/db/migration/V12__create_farm_table.sql
+++ src/main/resources/db/migration/V12__create_farm_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE farm(
     id INT NOT NULL AUTO_INCREMENT,
-    room_temp_and_humidity_sensor_id INT,
+    room_temp_and_humidity_sensor_id INT NULL,
     location VARCHAR(255) NOT NULL,
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
