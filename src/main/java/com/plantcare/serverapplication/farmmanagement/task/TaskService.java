89ea8ab0b7da4fb7adae60c2a2233f100a807d5e
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-09 13:16:36 +0800

    refactored the addtasks with appropriate business logic

--- src/main/java/com/plantcare/serverapplication/farmmanagement/task/TaskService.java
+++ src/main/java/com/plantcare/serverapplication/farmmanagement/task/TaskService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface TaskService {
 
-    List<TaskDto> addTasks(TaskOperationDto taskRequestDto, int containerId, int farmId);
+    List<TaskDto> addTasks(TaskOperationDto taskRequestDto, int farmId, int containerId);
 
     void deleteTasks(DeleteTasksDto deleteTasksDto, int containerId, int farmId);
 
