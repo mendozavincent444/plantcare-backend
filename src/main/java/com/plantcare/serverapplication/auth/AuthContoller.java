ecec4d5736e0c261ca9dcdbaa3eddfaf09db4d08
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-19 00:13:34 +0800

    moved changepassword from usercontroller to authcontroller

--- src/main/java/com/plantcare/serverapplication/auth/AuthContoller.java
+++ src/main/java/com/plantcare/serverapplication/auth/AuthContoller.java
@@ -2,11 +2,14 @@ package com.plantcare.serverapplication.auth;
 
 import com.plantcare.serverapplication.security.jwt.JwtUtils;
 import com.plantcare.serverapplication.security.service.UserDetailsImpl;
+import com.plantcare.serverapplication.security.service.UserDetailsPasswordServiceImpl;
+import com.plantcare.serverapplication.security.service.UserDetailsServiceImpl;
 import com.plantcare.serverapplication.shared.MessageResponseDto;
 import com.plantcare.serverapplication.shared.UserInfoResponseDto;
 import com.plantcare.serverapplication.usermanagement.role.Role;
 import com.plantcare.serverapplication.usermanagement.role.RoleEnum;
 import com.plantcare.serverapplication.usermanagement.role.RoleRepository;
+import com.plantcare.serverapplication.usermanagement.user.UpdatePasswordDto;
 import com.plantcare.serverapplication.usermanagement.user.User;
 import com.plantcare.serverapplication.usermanagement.user.UserRepository;
 import org.springframework.http.HttpHeaders;
@@ -27,6 +30,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthContoller {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
+    private final UserDetailsPasswordServiceImpl userDetailsPasswordService;
+    private final UserDetailsServiceImpl userDetailsService;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
@@ -34,11 +39,16 @@ public class AuthContoller {
     public AuthContoller(
             AuthenticationManager authenticationManager,
             UserRepository userRepository,
+            UserDetailsPasswordServiceImpl userDetailsPasswordService,
+            UserDetailsServiceImpl userDetailsService,
             RoleRepository roleRepository,
             PasswordEncoder passwordEncoder,
-            JwtUtils jwtUtils) {
+            JwtUtils jwtUtils
+    ) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
+        this.userDetailsPasswordService = userDetailsPasswordService;
+        this.userDetailsService = userDetailsService;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtils = jwtUtils;
@@ -109,5 +119,28 @@ public class AuthContoller {
         ResponseCookie cookie = this.jwtUtils.getCleanJwtCookie();
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
                 .body(new MessageResponseDto("You've been signed out!"));
+    }
+
+    @PostMapping("/update-password")
+    public ResponseEntity<?> changePassword(@RequestBody UpdatePasswordDto updatePasswordDto) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        UserDetailsImpl user = (UserDetailsImpl) this.userDetailsService.loadUserByUsername(userDetails.getUsername());
+
+        if (!this.passwordEncoder.matches(updatePasswordDto.getCurrentPassword(), user.getPassword())) {
+            return ResponseEntity.badRequest().body(new MessageResponseDto("Current password is incorrect. Please verify and try again."));
+        } else {
+            UserDetailsImpl updatedUserDetails = (UserDetailsImpl) this.userDetailsPasswordService
+                    .updatePassword(userDetails, this.passwordEncoder.encode(updatePasswordDto.getNewPassword()));
+
+            Authentication authentication = this.authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(updatedUserDetails.getUsername(), updatePasswordDto.getNewPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            ResponseCookie jwtCookie = this.jwtUtils.generateJwtCookie(updatedUserDetails);
+
+            return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                    .body(new MessageResponseDto("Password updated successfully!"));
+        }
     }
 }
