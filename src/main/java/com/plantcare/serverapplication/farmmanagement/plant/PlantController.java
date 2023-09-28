416ac026ff9a1a0783de4b8a088eafe3f5c3a066
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-28 14:17:52 +0800

    added allowcredetials to true

--- src/main/java/com/plantcare/serverapplication/farmmanagement/plant/PlantController.java
+++ src/main/java/com/plantcare/serverapplication/farmmanagement/plant/PlantController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
 @RequestMapping("api/v1/farms/{farmId}/plants")
 public class PlantController {
