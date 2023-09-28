bcf55eeb081e78bb73d50d6a574c79c28bfc5c1b
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-28 14:09:48 +0800

    crossorigin changes from wildcard to localhost:4200

--- src/main/java/com/plantcare/serverapplication/auth/AuthController.java
+++ src/main/java/com/plantcare/serverapplication/auth/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
