6d271aef267c0b40ccadb1a50bd0b1cd0edd2548
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-09-29 18:20:34 +0800

    removed @CrossOrigin annotation and added global cors configuration

--- src/main/java/com/plantcare/serverapplication/farmmanagement/farm/FarmController.java
+++ src/main/java/com/plantcare/serverapplication/farmmanagement/farm/FarmController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
 @RequestMapping("api/v1/farms")
 public class FarmController {
--- src/main/java/com/plantcare/serverapplication/usermanagement/user/UserController.java
+++ src/main/java/com/plantcare/serverapplication/usermanagement/user/UserController.java
@@ -19,7 +19,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
