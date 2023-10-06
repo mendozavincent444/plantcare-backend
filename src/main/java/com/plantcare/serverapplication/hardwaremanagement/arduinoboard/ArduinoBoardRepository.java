c1620f5d7f7a4ddb3adfc8c5f317906f2a3cbc99
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-10-06 18:56:59 +0800

    added feature to get all arduinoboards by farm id

--- src/main/java/com/plantcare/serverapplication/hardwaremanagement/arduinoboard/ArduinoBoardRepository.java
+++ src/main/java/com/plantcare/serverapplication/hardwaremanagement/arduinoboard/ArduinoBoardRepository.java
@@ -2,5 +2,11 @@ package com.plantcare.serverapplication.hardwaremanagement.arduinoboard;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 public interface ArduinoBoardRepository extends JpaRepository<ArduinoBoard, Integer> {
+
+    Optional<List<ArduinoBoard>> findAllByFarmId(int farmId);
 }
--- src/main/java/com/plantcare/serverapplication/hardwaremanagement/arduinoboard/ArduinoBoardService.java
+++ src/main/java/com/plantcare/serverapplication/hardwaremanagement/arduinoboard/ArduinoBoardService.java
@@ -1,9 +1,12 @@
 package com.plantcare.serverapplication.hardwaremanagement.arduinoboard;
 
+import java.util.List;
+
 public interface ArduinoBoardService {
 
     ArduinoBoardDto getArduinoBoardById(int arduinoBoardId);
 
     ArduinoBoardDto mapToDto(ArduinoBoard arduinoBoard);
 
+    List<ArduinoBoardDto> getAllArduinoBoardsByFarmId(int farmId);
 }
