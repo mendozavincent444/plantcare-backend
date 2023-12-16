f9616cb601efd61ae3e1c9bb80099209be4d2d66
Vincent Gabriel Mendoza <91596234+mendozavincent444@users.noreply.github.com>
2023-12-16 23:07:13 +0800

    Revert "generate different jwet cookie for web and mobile"

--- src/main/java/com/plantcare/serverapplication/usermanagement/user/UpdatePasswordDto.java
+++ src/main/java/com/plantcare/serverapplication/usermanagement/user/UpdatePasswordDto.java
@@ -1,16 +1,11 @@
 package com.plantcare.serverapplication.usermanagement.user;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdatePasswordDto {
-    @NotBlank
     String currentPassword;
-    @NotBlank
     String newPassword;
-    @NotBlank
-    String device;
 }
