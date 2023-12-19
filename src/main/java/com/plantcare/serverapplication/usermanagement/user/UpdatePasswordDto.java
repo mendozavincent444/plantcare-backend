6ec298048fd5395828e30ef4baf403e609d7349f
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-12-19 14:40:48 +0800

    change password validation on minimum lenght

--- src/main/java/com/plantcare/serverapplication/usermanagement/user/UpdatePasswordDto.java
+++ src/main/java/com/plantcare/serverapplication/usermanagement/user/UpdatePasswordDto.java
@@ -1,6 +1,7 @@
 package com.plantcare.serverapplication.usermanagement.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ public class UpdatePasswordDto {
     @NotBlank
     String currentPassword;
     @NotBlank
+    @Size(min = 5, max = 255)
     String newPassword;
     String device;
 }
