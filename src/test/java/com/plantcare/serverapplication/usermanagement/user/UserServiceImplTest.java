e1c2be2df6447afde39acc97aefe4a87133abf07
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2024-01-28 15:38:28 +0800

    added unit test for deactivating admin throws exception

--- src/test/java/com/plantcare/serverapplication/usermanagement/user/UserServiceImplTest.java
+++ src/test/java/com/plantcare/serverapplication/usermanagement/user/UserServiceImplTest.java
@@ -1,11 +1,14 @@
 package com.plantcare.serverapplication.usermanagement.user;
 
+import com.plantcare.serverapplication.exception.UserAccountStatusException;
 import com.plantcare.serverapplication.shared.UserDto;
 import com.plantcare.serverapplication.usermanagement.role.Role;
 import com.plantcare.serverapplication.usermanagement.role.RoleEnum;
 import com.plantcare.serverapplication.usermanagement.role.RoleRepository;
 import static org.assertj.core.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -85,5 +88,29 @@ public class UserServiceImplTest {
 
         assertThat(userDto.getId()).isEqualTo(user.getId());
         assertThat(userDto.isAccountNonLocked()).isEqualTo(false);
+    }
+
+    @DisplayName("Unit test for deactivating admin throws exception")
+    @Test
+    public void givenAdminId_whenDeactivatingAdmin_thenThrowsException() {
+        Role role = new Role(2, RoleEnum.ROLE_ADMIN);
+
+        User user = User.builder()
+                .id(1)
+                .email("benjamin@yahoo.com")
+                .isAccountNonLocked(false)
+                .username("admin")
+                .firstName("Benjamin")
+                .lastName("Brown")
+                .role(role)
+                .password("sample")
+                .isAllowNotifications(true)
+                .build();
+
+        given(this.userRepository.findById(1)).willReturn(Optional.of(user));
+
+        assertThrows(UserAccountStatusException.class, () -> this.userServiceImpl.deactivateAdmin(1));
+
+        verify(this.userRepository, never()).save(any(User.class));
     }
 }
